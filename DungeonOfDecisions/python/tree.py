5886d7ba9afc3089eabc84341ab7e96b79fe554a
Edison Rodrigo Soto Perez <edison.soto@epn.edu.ec>
2026-01-25 15:19:49 -0500

    Reenlazado con el main

--- DungeonOfDecisions/python/tree.py
+++ DungeonOfDecisions/python/tree.py
@@ -1,13 +1,14 @@
 class DecisionNode:
     def __init__(self, description):
-        self.description = description,
-        self.left = None,
-        self.right = None,
+        self.description = description
+        self.left = None
+        self.right = None
 
-        def build_decision_tree():
-            root = DecisionNode("¿Pelear o huir?")
-            root.left = DecisionNode("Pelear")
-            root.right = DecisionNode("Huir")
-            root.left.left = DecisionNode("Atacar")
-            root.left.right = DecisionNode("Defender")
-            return root
+
+def build_decision_tree():
+    root = DecisionNode("¿Pelear o huir?")
+    root.left = DecisionNode("Pelear")
+    root.right = DecisionNode("Huir")
+    root.left.left = DecisionNode("Atacar")
+    root.left.right = DecisionNode("Defender")
+    return root
