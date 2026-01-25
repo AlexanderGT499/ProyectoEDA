2f9bc647414793e06eff4a4321fa227630ca73d0
alxnfc1805 <alex.pogo1805@gmail.com>
2026-01-24 19:29:25 -0500

    Alex (#3)
    * Se modificaron las clases para testing
    
    * Se modificaron las clases para testing 2

---
+++ DungeonOfDecisions/python/tree.py
@@ -0,0 +1,13 @@
+class DecisionNode:
+    def __init__(self, description):
+        self.description = description,
+        self.left = None,
+        self.right = None,
+
+        def build_decision_tree():
+            root = DecisionNode("¿Pelear o huir?")
+            root.left = DecisionNode("Pelear")
+            root.right = DecisionNode("Huir")
+            root.left.left = DecisionNode("Atacar")
+            root.left.right = DecisionNode("Defender")
+            return root
